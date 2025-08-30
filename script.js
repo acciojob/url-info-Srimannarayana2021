f48da4029cab3e44f58152b29dd43508d568769d
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-08-30 12:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+    function showUrlLength() {
+      // get current URL
+      let url = location.href;
+      // get length
+      let length = url.length;
+      // show alert
+      alert("The length of the URL is: " + length);
+    }
